379fa9a586beb8bd6d5e66452d4a264668a7110a
Eddie Lopez <iameddielopez@gmail.com>
2020-07-17 14:08:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash[key = :key]
+  hash[:key] = key
+  hash[:value] = value
   hash
   end
 
